87ba1ed79567f7de38296d45701ea658a315b640
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-11-03 09:07:29 +0100

    Improvements in Recursively Graph extraction and incorrect answers selection

--- base/static/js/GameMining/mining.js
+++ base/static/js/GameMining/mining.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
+  
+  
   String.prototype.replaceAll = function(str1, str2, ignore) 
   {
 	  return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
   }
+
+          /*
+      $('#extract-fb').click(function() { 
+          /*$.blockUI({ css: { 
+              border: 'none', 
+              padding: '15px', 
+              backgroundColor: '#000', 
+              '-webkit-border-radius': '10px', 
+              '-moz-border-radius': '10px', 
+              opacity: .5, 
+              color: '#fff' 
+          } });
+          }); 
+   */
+
+
+ 
+
+
   GraphExtractor.init();
   GraphManager.init();
   $("#second").hide();
@@ -10,12 +31,10 @@ $(document).ready(function(){
   $(".chzn-select").chosen();
 
   $('#search-fb').click(function(){
-  $("#second").show();
     GraphExtractor.search();
   });
   
   $('#extract-fb').click(function(){
-  $("#third").show();
     GraphExtractor.get();
   });
   
@@ -30,23 +49,23 @@ $(document).ready(function(){
   $('#create-quiz-fb').click(function(){
     $("#create-quiz-fb").hide();
     QuizConstructor.createQuiz("FREEBASE_GRAPH")
-  });
+    });
+
+    $('#answer0').click(function(){
+      QuizConstructor.checkAnswer(0);
+    });
+    $('#answer1').click(function(){
+      QuizConstructor.checkAnswer(1);
+    });
+    $('#answer2').click(function(){
+      QuizConstructor.checkAnswer(2);
+    });
+    $('#answer3').click(function(){
+      QuizConstructor.checkAnswer(3);
+    });
   
-        $('#answer0').click(function(){
-        QuizConstructor.checkAnswer(0);
-      });
-        $('#answer1').click(function(){
-        QuizConstructor.checkAnswer(1);
-      });
-        $('#answer2').click(function(){
-        QuizConstructor.checkAnswer(2);
-      });
-            $('#answer3').click(function(){
-        QuizConstructor.checkAnswer(3);
-      });
-      
-        $('#answer0').hide();
-        $('#answer1').hide();
-        $('#answer2').hide();
-        $('#answer3').hide();
+    $('#answer0').hide();
+    $('#answer1').hide();
+    $('#answer2').hide();
+    $('#answer3').hide();
 });
