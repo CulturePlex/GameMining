fe90d84da27a50f84b61ba43ceeeb060d8696fc4
pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-12-05 04:37:21 +0100

    Added DBpedia query system

--- base/static/js/GameMining/QuizConstructor.js
+++ base/static/js/GameMining/QuizConstructor.js
@@ -12,6 +12,8 @@ var QuizConstructor = {
   treshold: 100,
   bigJson: null,
   answerIndex: null,
+  infoLeaf:null,
+  infoEdge:null,
     
   contains: function(a, obj) {
     for (var i = 0; i < a.length; i++) {
@@ -143,81 +145,110 @@ var QuizConstructor = {
       }
   },
   setQuiz: function(graph_type){
-		      $('#play-msg').hide();
-              if(graph_type == "FREEBASE_GRAPH")
-              {
-                  var question = 'Which of the followings has <em>' + QuizConstructor.value +'</em> as ' + QuizConstructor.attrib.replaceAll("_"," ") +'?';
-              }
-              if(graph_type == "CULTUREPLEX_GRAPH")
-              {
-                  var question = 'Which of the following is <em>' + attrib + '</em> ' + value +'?';
-              }
-              var quiz = '<h>' + question + '</h2>';
-              
-              //Deleting duplicated answers
-              var j = 0;
-              while ( j < QuizConstructor.answers.length )
-              {
-                  if ($.inArray(QuizConstructor.answers[j], QuizConstructor.answers)!== j)
-                  {
-                      QuizConstructor.answers.splice(j,2);
-                      console.log('repetido');
-                  }
-                  if (QuizConstructor.answers[j]==undefined)
-                  {
-                      QuizConstructor.answers.splice(j,2);
-                      console.log('repetido');
-                  }
-                  j++;
-              }
-              console.log(QuizConstructor.answers);
-              //If there is only one, restart quiz
-              if(QuizConstructor.answers.length<2 || QuizConstructor.contains(QuizConstructor.answers,undefined))
-              {
-                  QuizConstructor.createQuiz(graph_type);
-              }
-              else
-              {
-                
-                //Mixing answers
-                var answersmixed = [];
-                j = 0;
-                while ( j < QuizConstructor.answers.length )
-                {
-                    var r = parseInt(Math.random(10)*QuizConstructor.answers.length);
-                    if ( typeof(answersmixed[r] ) === "undefined")
-                    {
-                        answersmixed[r] = QuizConstructor.answers[j];
-                        j++;
-                    }
-                }
-                for (var i = 0 ; i < 4; i++)
-                {
-                    $('#answer'+i).removeClass("btn-success");
-                    $('#answer'+i).removeClass("btn-danger");
-                    
-                    $('#answer'+i).hide();
-                }
-                $('#answer0').hide();
-                $('#answer1').hide();
-                $('#answer2').hide();
-                $('#answer3').hide();
-                for (var i = 0 ; i < QuizConstructor.answers.length; i++)
-                {
-                    $('#answer'+i).html(answersmixed[i]);
-                    $('#answer'+i).show();
-                }
-                
-                if($('#answer1').html=="Create Quiz!"){$('#answer1').hide();}
-                if($('#answer0').html=="Create Quiz!"){$('#answer0').hide();}
-                if($('#answer2').html=="Create Quiz!"){$('#answer2').hide();}
-                if($('#answer3').html=="Create Quiz!"){$('#answer3').hide();}
+    $('.results').hide();
+	console.log(QuizConstructor.value);
+	WIKIPEDIA.getData('http://en.wikipedia.org/wiki/'+QuizConstructor.value.replaceAll(" ","_"),
+		function (result){
 
-                //quiz += '<div>' + 'Correct Answer :' + QuizConstructor.correct + '</div>';
-                $('.quizViewer').empty();
-                $('.quizViewer').append(quiz);
-                //$.unblockUI();
-              }
+			QuizConstructor.infoLeaf = result;
+		    QuizConstructor.leafHtml = "<a href='#' id='leafLink'>"+QuizConstructor.value+"</a>";
+
+			QuizConstructor.edgeHtml = QuizConstructor.attrib.replaceAll("_"," ");
+			finishQuiz();
+		},
+		function (){
+			QuizConstructor.infoLeaf = "error";
+			QuizConstructor.edgeHtml = QuizConstructor.attrib.replaceAll("_"," ");
+		    QuizConstructor.leafHtml = QuizConstructor.value;
+			finishQuiz();
+			console.log('error!');
+
+
+
+		}
+	  );				
+	  finishQuiz= function () {
+
+      $('#play-msg').hide();
+
+      if(graph_type == "FREEBASE_GRAPH")
+      {
+          var question = 'Which of the followings has <em>' + QuizConstructor.leafHtml +'</em> as ' + QuizConstructor.edgeHtml +'?';
+      }
+      if(graph_type == "CULTUREPLEX_GRAPH")
+      {
+          var question = 'Which of the followings has <em>' + QuizConstructor.leafHtml +'</em> as ' + QuizConstructor.edgeHtml +'?';
+      }
+      var quiz = '<h>' + question + '</h2>';
+      
+      //Deleting duplicated answers
+      var j = 0;
+      while ( j < QuizConstructor.answers.length )
+      {
+          if ($.inArray(QuizConstructor.answers[j], QuizConstructor.answers)!== j)
+          {
+              QuizConstructor.answers.splice(j,2);
+              console.log('repetido');
+          }
+          if (QuizConstructor.answers[j]==undefined)
+          {
+              QuizConstructor.answers.splice(j,2);
+              console.log('repetido');
+          }
+          j++;
+      }
+      console.log(QuizConstructor.answers);
+      //If there is only one, restart quiz
+      if(QuizConstructor.answers.length<2 || QuizConstructor.contains(QuizConstructor.answers,undefined))
+      {
+          QuizConstructor.createQuiz(graph_type);
+      }
+      else
+      {
+        
+        //Mixing answers
+        var answersmixed = [];
+        j = 0;
+        while ( j < QuizConstructor.answers.length )
+        {
+            var r = parseInt(Math.random(10)*QuizConstructor.answers.length);
+            if ( typeof(answersmixed[r] ) === "undefined")
+            {
+                answersmixed[r] = QuizConstructor.answers[j];
+                j++;
+            }
+        }
+        for (var i = 0 ; i < 4; i++)
+        {
+            $('#answer'+i).removeClass("btn-success");
+            $('#answer'+i).removeClass("btn-danger");
+            
+            $('#answer'+i).hide();
+        }
+        $('#answer0').hide();
+        $('#answer1').hide();
+        $('#answer2').hide();
+        $('#answer3').hide();
+        for (var i = 0 ; i < QuizConstructor.answers.length; i++)
+        {
+            $('#answer'+i).html(answersmixed[i]);
+            $('#answer'+i).show();
+        }
+        
+        if($('#answer1').html=="Create Quiz!"){$('#answer1').hide();}
+        if($('#answer0').html=="Create Quiz!"){$('#answer0').hide();}
+        if($('#answer2').html=="Create Quiz!"){$('#answer2').hide();}
+        if($('#answer3').html=="Create Quiz!"){$('#answer3').hide();}
+
+        //quiz += '<div>' + 'Correct Answer :' + QuizConstructor.correct + '</div>';
+        $('.quizViewer').empty();
+        $('.quizViewer').append(quiz);
+		$("a#leafLink").click(function(){
+			InfoManager.display(QuizConstructor.infoLeaf);
+		});
+        //$.unblockUI();
+      }
+  	};
   },
   checkAnswer: function(i) {
     console.log($('#quizViewer'));
